c3841832ea27754e9fadb8ac090eb4afb8e95ae2
Yuri <jekonik26@gmail.com>
2018-06-24 19:24:45 +0300

    work version

--- src/components/items/SiteItem.jsx
+++ src/components/items/SiteItem.jsx
@@ -2,13 +2,50 @@ import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { Filters } from './'
 import '../../styles/SiteItem.scss'
+import OwlCarousel from 'react-owl-carousel2';
+import '../../styles/SelectItem.scss'
 
 class SiteItem extends Component {
   constructor (props) {
     super(props)
   }
+
+  componentDidMount () {
+    this.setState({ opened: this.props.opened })
+  }
+  handleDropDownClick = () => {
+    const { opened } = this.state
+    if (opened) {
+      this.setState({ opened: false })
+    } else {
+      this.setState({ opened: true })
+      // document.onclick = () => this.setState({ opened: false })
+    }
+  }
+  handleCheckboxClick = ({ target: { checked, name, id } }) => {
+    const { group } = this.state
+    const { set, onListChange } = this.props
+    let _newGroup = group
+    this.setState({checked__filter:{ target: { checked, name }}})
+    if (checked) {
+      _newGroup.push(set[name])
+      this.setState({ group: _newGroup }, () => {
+        console.log(this.state.group)
+      })
+    } else {
+      _newGroup = _newGroup.filter(item => id != item.ID)
+      this.setState({ group: _newGroup }, () => {
+        console.log(this.state.group)
+      })
+    }
+    onListChange(_newGroup)
+  }
+
+
+
   render () {
     const { site, url } = this.props
+    console.log(this.props)
     return (
       <Link to={`${url}/${site.ID}`} className='siteItem'>
         <p className='name'>{ site.Name }</p>
